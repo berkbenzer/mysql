22513fee15f1ed200fa71dc6cea389a981a97840
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2023-12-20 10:34:08 +0100

    Create sql_operations.sql

---
+++ sql_operations.sql
@@ -0,0 +1,16 @@
+UPDATE python_test.table_name 
+SET START_TIME = STR_TO_DATE('20-12-2023' ,'%d-%m-%Y')
+WHERE  filter in ('data') 
+and filter = 'data'
+
+
+select  date_format(start_time, '%d-%m-%Y') as new_time  from python_test.ltable_name
+where 1=1
+having new_time = '20-12-2023'; 
+
+
+
+select  date_format(date_sub(start_time, INTERVAL 1 DAY), '%d-%m-%Y') as int_time  from python_test.table_name
+where 1=1
+and column in ('data1','data2')
+order by int_time desc
